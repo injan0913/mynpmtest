7df1899532ed1552c6b93e49f45042c4e0c22dd1
tonyw <tonyw@hp.com>
2016-07-13 16:05:11 +0800

    package initailize

---
+++ test/index.js
@@ -0,0 +1,8 @@
+import assert from 'assert';
+import heyinjan0913Xoxo from '../lib';
+
+describe('heyinjan-0913-xoxo', function () {
+  it('should have unit test!', function () {
+    assert(false, 'we expected this package author to add actual unit tests.');
+  });
+});
