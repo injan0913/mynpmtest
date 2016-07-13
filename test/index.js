cad09d92bb1472dc88918a2df2f32182c6367865
tonyw <tonyw@hp.com>
2016-07-13 16:34:38 +0800

    travis CI

--- test/index.js
+++ test/index.js
@@ -1,8 +1,7 @@
-import assert from 'assert';
 import heyinjan0913Xoxo from '../lib';
 
 describe('heyinjan-0913-xoxo', function () {
   it('should have unit test!', function () {
-    assert(false, 'we expected this package author to add actual unit tests.');
+    heyinjan0913Xoxo.test();
   });
 });
